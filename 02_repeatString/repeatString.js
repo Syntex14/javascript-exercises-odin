ff667f68940c2c28878f6e5cbbd981b849664129
Syntex14 <rangelabraham14@gmail.com>
2023-03-14 12:46:06 -0500

    Finish practice problem #2, a bit messy, needs some touch up

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    const sumArray = [];
+    if (Math.sign(num) === -1){
+        return "ERROR";
+    }
+    for (let i = 0; i < num; ++i) {
+        sumArray.push(string);
+    }
+    return sumArray.join("");
 
+    // create a loop that iterate three times in a row
+    // create an array that holds the string
+    // Once loop is completed, join array together
+    // return string of array.
 };
 
 // Do not edit below this line

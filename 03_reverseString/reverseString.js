eba5e70259f0ac11741522c488f4e5dadc4371bc
Syntex14 <rangelabraham14@gmail.com>
2023-03-14 13:20:27 -0500

    Finish practice problem #3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
-};
+const reverseString = function(string) {
+    // split the string and store in an array
+    // reverse the array
+    // join the array
+    return string.split("").reverse().join("");
+};  
 
 // Do not edit below this line
 module.exports = reverseString;

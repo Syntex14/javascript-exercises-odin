6b581a31701f3fe3dea2e8c7efa742ccbe85cbfc
Syntex14 <rangelabraham14@gmail.com>
2023-04-10 12:57:54 -0500

    Finish practice problem #8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -3,28 +3,48 @@ const add = (num1, num2) => {
 };
 
 
-const subtract = function(num1, num2) {
-	// initial value = 0;
-    // return initial value = num1 - num2;
+const subtract = (num1, num2) => {
+    return num1 - num2;
 };
 
 const sum = function(array) {
   // usage of reduce function
-    // array.reduce((total, num) => {
-      // return total + num; 
-    // })
+    const sumTotal = array.reduce((total, num) => {
+      return total + num; 
+    }, 0);
+    return sumTotal;
  };
 
-const multiply = function() {
 
+function multiply(array) {
+  const sumTotal = array.reduce((total, num) => {
+    return total * num; 
+  });
+  return sumTotal;
+};
+        
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const power = function() {
-	
-};
+const factorial = function(num) {
+	// how to create the range of numbers for the factorial
+    // const x = [];
+    // for (i = 1; i <= num; i++ )
+      // x.push[i];
+      if (num === 0) {
+        return 1;
+      }
+    const x = [];
+      for(i = 1; i <= num; i++) {
+        x.push(i);
+      }
+    const sumTotal = x.reduce((total, factorial) => {
+      return total * factorial; 
+      });
 
-const factorial = function() {
-	
+      return sumTotal;
+    
 };
 
 // Do not edit below this line

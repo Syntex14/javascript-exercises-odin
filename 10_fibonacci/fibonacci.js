296ad683e9d2d2f5e62f478d3f356f66708fbb92
Syntex14 <rangelabraham14@gmail.com>
2023-05-11 16:11:57 -0500

    Finished problem, Odin's solution is much better.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    //
+    // n2 = 1
+    // array = [1]
+    // for (i = 0; i < num; i++) 
+    // n1 = array[0]
+    // n2 += n1;
+    if (Math.sign(num) === -1 ) {
+        return "OOPS";
+    }
+    
+    const array = [0, 1];
+    for (i = 0; i < num; i++) {
+    let numberPredicingOne = array[i];
+    let numberPredicingTwo = array[i+1];
+       array.push(numberPredicingTwo += numberPredicingOne);
 
+    }
+    return array[num];
 };
 
 // Do not edit below this line

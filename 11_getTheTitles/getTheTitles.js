1287cb9c2f4a9e5c5d659a908b9ff974b3c0bcf8
Syntex14 <rangelabraham14@gmail.com>
2023-05-12 16:05:40 -0500

    Completed practice problem. Definitely different from Odin's solution. Odin's solution is superior but I used methods and techniques that were familiar with me.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,19 @@
-const getTheTitles = function() {
+const getTheTitles = function(arrayObjs) {
+    let key = "title";
+    const resultArray = [];
+    for (i = 0; i < arrayObjs.length; i++) {
+        resultArray.push(arrayObjs[i][key]);
+           
+    }   
+    return resultArray;
 
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+// key = "title";
+
+// reference the array index you are attempting to get
+    // reference the key or key value
+        // arrayObjs[0]

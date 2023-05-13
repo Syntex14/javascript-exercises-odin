7152c212ce4dbb79d780ffd499216932d1a884b4
Syntex14 <rangelabraham14@gmail.com>
2023-05-12 21:19:39 -0500

    Incomplete program. Still needs to return the answer to the function.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const oldestObject = array.reduce((acc, val) => {
+        acc[val.name] = val.yearOfDeath - val.yearOfBirth;
+        return acc;
+    }, {});
+    return Math.max(oldestObject[])
 };
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
